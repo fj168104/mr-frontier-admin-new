fd27cbad9a4010795ab88b8093090d21259a08bc
ppd-03020234 <fengj@jxltech.com>
2018-09-14 10:08:53 +0800

    css update !

--- src/main/java/com/jk/modules/sys/controller/IndexController.java
+++ src/main/java/com/jk/modules/sys/controller/IndexController.java
@@ -1,8 +1,11 @@
 package com.jk.modules.sys.controller;
 
 import com.feilong.core.DatePattern;
+import com.github.pagehelper.PageInfo;
 import com.jk.common.base.controller.BaseController;
 import com.jk.common.util.ShiroUtils;
+import com.jk.modules.log.model.Log;
+import com.jk.modules.log.service.LogService;
 import com.jk.modules.sys.model.Permission;
 import com.jk.modules.sys.model.User;
 import com.jk.modules.sys.service.PermissionService;
@@ -12,6 +15,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.annotation.Resource;
 import java.text.DateFormat;
@@ -29,6 +33,8 @@ public class IndexController extends BaseController{
 
     @Resource
     private PermissionService permissionService;
+    @Resource
+    private LogService logService;
 
     /*
      * @methodName: toIndex
@@ -49,9 +55,21 @@ public class IndexController extends BaseController{
      * @return
      */
     @GetMapping(value = "/admin/index")
-    public String index(ModelMap modelMap){
-
-        return BASE_PATH + "index";
+    public String list(
+            @RequestParam(value = "pageNum", defaultValue = "1") Integer pageNum,
+            String username, String startTime, String endTime, ModelMap modelMap) {
+        try {
+            log.debug("分页查询日志列表参数! pageNum = {}, username = {}, username = {}, startTime = {}, endTime = {}", pageNum, username, startTime, endTime);
+            PageInfo<Log> pageInfo = logService.findPage(pageNum, PAGESIZE, username, startTime, endTime);
+            log.info("分页查询日志列表结果！ pageInfo = {}", pageInfo);
+            modelMap.put("pageInfo", pageInfo);
+            modelMap.put("username", username);
+            modelMap.put("startTime", startTime);
+            modelMap.put("endTime", endTime);
+        } catch (Exception e) {
+            log.error("分页查询日志列表失败! e = {}", e);
+        }
+        return BASE_PATH + "log/log-list";
     }
 
     /**
